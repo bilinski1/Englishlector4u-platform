eaa00bbcc1d369854cbccbbf3995a9ea84f2ef2d
bilinski1 <bilinski.mateusz@gmail.com>
2019-12-30 21:59:37 +0100

    intsallin ejs

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 
 app.set('port', process.env.PORT || 3000);
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "views"));
 
 app.get('/', function (req, res) {
-    res.send('hello world')
+    res.render('index');
   })
 
 app.listen(port, () => {
-    console.log("Server running on port 3000");
+    console.log("Server running on port 3000 or online ;)");
    });
 
