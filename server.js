e7e8373e44a247224814ef44a1097e0ba9ba18cf
bilinski1 <bilinski.mateusz@gmail.com>
2020-01-14 14:43:12 +0100

    changes

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 import express from 'express';
-import { resolve } from 'path';
+//import { resolve } from 'path';
 import { PORT, MONGODB_URI } from './config';
 import bodyParser from 'body-parser';
 const app = express();
+// Setting Port, View Engine 
+//app.set('port', process.env.PORT || 3000);
+app.set("view engine", "ejs");
+app.set("views", resolve(__dirname, "views"));
 // Setting up database mongoose, mongoDB
 import { connect } from 'mongoose';
 // Conncecting to database
 app.listen(PORT, () => {
-  connect(MONGODB_URI, { useNewUrlParser: true}, {useMongoClient: true},
-    );
-  console.log("Server running on port 3000 or online ;)");
+  connect(MONGODB_URI, { useNewUrlParser: true}, {useMongoClient: true})
 });
-// Setting Port, View Engine 
-app.set('port', process.env.PORT || 3000);
-app.set("view engine", "ejs");
-// Setting Location "views" for EJS 
-app.set("views", resolve(__dirname, "views"));
 app.get('/', function (req, res) {
     res.render('index');
   })
