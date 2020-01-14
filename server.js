6f8036a13f70075295008e82364693f2e7ce4f91
bilinski1 <bilinski.mateusz@gmail.com>
2020-01-14 14:09:19 +0100

    database added

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const app = express();
-
+// Setting up database mongoose, mongoDB
+const mongoose = require('mongoose');
+// Conncecting to database
+mongoose.connect('mongodb+srv://el4u:bilisw362@el4u-fc1rw.gcp.mongodb.net/test?retryWrites=true&w=majority', {
+  useMongoClient: true
+}
+);
 // Setting Port, View Engine 
 app.set('port', process.env.PORT || 3000);
 app.set("view engine", "ejs");
