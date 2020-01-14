1cf4e5696437a9a2ca053a1ca766af5139d4ff45
bilinski1 <bilinski.mateusz@gmail.com>
2020-01-14 12:54:37 +0100

    body-parser set up

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const app = express();
 
+// Setting Port, View Engine 
 app.set('port', process.env.PORT || 3000);
 app.set("view engine", "ejs");
+// Setting Location "views" for EJS 
 app.set("views", path.resolve(__dirname, "views"));
+
 
 app.get('/', function (req, res) {
     res.render('index');
