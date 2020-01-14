0223e4748205f3b99e0528bc202dcdc3b5077a91
bilinski1 <bilinski.mateusz@gmail.com>
2020-01-14 15:25:04 +0100

    asdf

--- serv3er.js
+++ serv3er.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
-const app = express();
+const { ENV, URL, PORT, MONGODB_URI } = require('./config');
+
 
 // Setting Port, View Engine 
 app.set('port', process.env.PORT || 3000);
@@ -12,9 +12,14 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "views"));
 
 
+//import { connect } from 'mongoose';
+//app.listen(config.PORT, () => {
+  //connect(MONGODB_URI, { useNewUrlParser: true}, {useMongoClient: true})
+//});
+
 app.get('/', function (req, res) {
-    res.render('index');
-  })
+  res.render('index');
+})
 app.listen(port, () => {
-    console.log("Server running on port 3000 or online ;)");
-   });
+  console.log("Server running on port 3000 or online ;)");
+ });
