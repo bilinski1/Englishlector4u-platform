567427e20664ecaebe68e6cf9335c9a0ccbe68fa
bilinski1 <bilinski.mateusz@gmail.com>
2020-01-14 15:13:22 +0100

    port

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.set("views", resolve(__dirname, "views"));
 //static files
 app.use(express.static(path.join(__dirname, 'public')));
 //import { connect } from 'mongoose';
+http.listen((process.env.PORT || 5000), function(){
+  console.log('listening on *:5000');
+});
+
 app.listen(config.PORT, () => {
   connect(MONGODB_URI, { useNewUrlParser: true}, {useMongoClient: true})
 });
