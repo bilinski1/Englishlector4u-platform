4daa4a7c74db3e8872bed713667434f335cd38f8
bilinski1 <bilinski.mateusz@gmail.com>
2020-01-14 16:06:38 +0100

    asdf

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
+const port = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-const { ENV, URL, PORT, MONGODB_URI } = require('./config');
+//const { ENV, URL, PORT, MONGODB_URI } = require('config');
 
 
 // Setting Port, View Engine 
 app.set('port', process.env.PORT || 3000);
 app.set("view engine", "ejs");
+
 // Setting Location "views" for EJS 
 app.set('views', path.join(__dirname, '../views'));
-
 
 //import { connect } from 'mongoose';
 //app.listen(config.PORT, () => {
