c541f23c69383728024bbeed77d999757dc65557
bilinski1 <bilinski.mateusz@gmail.com>
2020-01-14 15:13:35 +0100

    port

--- server.js
+++ server.js
@@ -3,16 +3,12 @@ const app = express();
 const { resolve } = require('path');
 const bodyParser = require('body-parser');
 const { ENV, URL, PORT, MONGODB_URI } = require('./config');
-//app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 app.set("view engine", "ejs");
 app.set("views", resolve(__dirname, "views"));
 //static files
 app.use(express.static(path.join(__dirname, 'public')));
 //import { connect } from 'mongoose';
-http.listen((process.env.PORT || 5000), function(){
-  console.log('listening on *:5000');
-});
-
 app.listen(config.PORT, () => {
   connect(MONGODB_URI, { useNewUrlParser: true}, {useMongoClient: true})
 });
