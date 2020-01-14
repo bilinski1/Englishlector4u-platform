ad12af3f5b244b99697372fe8367493d64b3795e
bilinski1 <bilinski.mateusz@gmail.com>
2020-01-14 15:09:51 +0100

    fix

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const express = 'express';
-const { resolve } = 'path';
-const { ENV, URL, PORT, MONGODB_URI } = './config';
-const bodyParser = 'body-parser';
+const express = require('express');
 const app = express();
-//app set
-app.set('port', process.env.PORT || 3000);
+const { resolve } = require('path');
+const bodyParser = require('body-parser');
+const { ENV, URL, PORT, MONGODB_URI } = require('./config');
+//app.set('port', process.env.PORT || 3000);
 app.set("view engine", "ejs");
 app.set("views", resolve(__dirname, "views"));
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
 //import { connect } from 'mongoose';
+app.listen(config.PORT, () => {
+  connect(MONGODB_URI, { useNewUrlParser: true}, {useMongoClient: true})
+});
 app.get('/', function (req, res) {
     res.render('index')
-  });
-  app.listen(PORT, () => {
-    connect(MONGODB_URI, { useNewUrlParser: true}, {useMongoClient: true})
-  });
+});
