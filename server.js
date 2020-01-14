f5c3b65290ff13e440c6423f1979d24705ad1045
bilinski1 <bilinski.mateusz@gmail.com>
2020-01-14 15:30:16 +0100

    sdf

--- server.js
+++ server.js
@@ -2,19 +2,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
-const bodyParser = require('body-parser');
-const app = express();
 
-// Setting Port, View Engine 
 app.set('port', process.env.PORT || 3000);
 app.set("view engine", "ejs");
-// Setting Location "views" for EJS 
 app.set("views", path.resolve(__dirname, "views"));
 
-
 app.get('/', function (req, res) {
+    res.send('hello world')
     res.render('index');
   })
+
 app.listen(port, () => {
+    console.log("Server running on port 3000");
     console.log("Server running on port 3000 or online ;)");
    });
